ca9bf6d84f225a90c275023c614011dcc8e93025
Joan Rosell Rodríguez <joan.rosell@col.vueling.com>
2021-11-25 18:07:11 +0100

    feat: add basic aliases

--- home/profile.ps1
+++ home/profile.ps1
@@ -1,1 +1,22 @@
-Write-Output "Hello World!"
+function GoToDevHome {
+    Set-Location 'C:\dev\'
+}
+
+function GoToVyHome {
+    Set-Location 'C:\VuelingApps\Apps'
+}
+
+function Find-SolutionFile {
+    return Get-ChildItem . | Where-Object {$_.FullName -match ".sln$"}
+}
+
+function Open-VisualStudio {
+    $solution = Find-SolutionFile
+    Write-Output "Opening Solution: $solution"
+    explorer.exe .
+    Start-Process devenv $solution
+}
+
+Set-Alias 'dev' GoToDevHome
+Set-Alias 'vy' GoToVyHome
+Set-Alias 'svs' Open-VisualStudio
